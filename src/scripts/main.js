9af760a5d2536abec39cfa104ccdb00e75d66ebd
Mestre dos Magos <ferminoapolinariojuliano@gmail.com>
2025-06-10 14:34:29 -0300

    criando o timer do evento

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
 AOS.init();
+
+const dateFutura = new Date("June 18, 2025 19:30:00");
+const timeStampDataFuturo = dateFutura.getTime();
+
+console.log('data futura',timeStampDataFuturo);
+
+setInterval(function(){
+    const dataAtual = new Date();
+    const timeSTampDataAtual = dataAtual.getTime();
+    const dataEvent = timeStampDataFuturo - timeSTampDataAtual;
+
+    const dia = 1000 * 60 * 60 * 24;
+    const hora = 1000 * 60 * 60;
+    const minuto = 1000 * 60
+    const segundos = 1000;
+
+    const dataEmDias = Math.floor( dataEvent / dia);
+    const dataEmHoras = Math.floor(dataEvent % (dia) / (hora));
+    const dataEmMinutos = Math.floor(dataEvent  % (hora) / (minuto));
+    const dataEmSegundos = Math.floor(dataEvent % (minuto) / segundos);
+
+    document.querySelector('.contagem-regressiva').innerHTML = `${dataEmDias}d ${dataEmHoras}h ${dataEmMinutos}m ${dataEmSegundos}s`;
+
+    if(timeSTampDataAtual > timeStampDataFuturo){
+        document.querySelector('.contagem-regressiva').innerHTML ="O evento já começou";
+    }
+},1000)
